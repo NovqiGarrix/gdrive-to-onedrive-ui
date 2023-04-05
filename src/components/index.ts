bae885df5e221cb182264c1ad9d9a34cfd00f265
NovqiGarrix <novqigarrix@gmail.com>
2023-04-05 20:21:36 +0700

    feat: organize exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,14 +1,12 @@
 export { default as Navbar } from './Navbar';
 export { default as Sidebar } from './Sidebar';
-export { default as UploadArea } from './UploadArea';
 export { default as Folders } from './Folders';
-
-export { default as FilesContainer } from './FilesContainer';
-export { default as DeleteFilesModal } from './DeleteFilesModal';
-export { default as FilesContainerWrapper } from './FilesContainerWrapper';
-
-export { default as AccountSettings } from './AccountSettings';
+export { default as UploadArea } from './UploadArea';
 export { default as LoadingIcon } from './LoadingIcon';
 export { default as Breadcrumbs } from './Breadcrumbs';
 export { default as BeautifulError } from './BeautifulError';
+export { default as FilesContainer } from './FilesContainer';
+export { default as AccountSettings } from './AccountSettings';
+export { default as DeleteFilesModal } from './DeleteFilesModal';
+export { default as FilesContainerWrapper } from './FilesContainerWrapper';
 export { default as DisconnectedProviderAccount } from './DisconnectedProviderAccount';
