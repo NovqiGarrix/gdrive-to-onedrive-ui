114dc9df1b931e9ffcad4c4508ed92cd4e3fa47c
NovqiGarrix <novqigarrix@gmail.com>
2023-05-16 15:49:07 +0700

    fix: types error

--- src/components/FilesContainerWrapper.tsx
+++ src/components/FilesContainerWrapper.tsx
@@ -1,10 +1,5 @@
 import { FunctionComponent, useRef } from "react";
-
 import dynamic from "next/dynamic";
-import { shallow } from "zustand/shallow";
-
-import useBeforeUnload from "../hooks/useBeforeUnload";
-import useUploadInfoProgress from "../hooks/useUploadInfoProgress";
 
 import FilesContainer from "./FilesContainer";
 
@@ -12,17 +7,6 @@ const DeleteFilesModal = dynamic(() => import("./DeleteFilesModal"));
 
 const FilesContainerWrapper: FunctionComponent = () => {
   const ref = useRef<HTMLDivElement>(null);
-
-  const uploadInfoProgress = useUploadInfoProgress(
-    (s) => s.uploadInfoProgress,
-    shallow
-  );
-
-  useBeforeUnload(() => {
-    uploadInfoProgress.forEach(({ abortController }) => {
-      abortController.abort();
-    });
-  });
 
   return (
     <div ref={ref} className="w-full">
