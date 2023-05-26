2a5a6e9c0a5dafb197c156045ac6d774f54f731a
NovqiGarrix <novqigarrix@gmail.com>
2023-05-26 16:06:15 +0700

    fix: types error

--- src/hooks/useUnConnectedTransferModal.ts
+++ src/hooks/useUnConnectedTransferModal.ts
@@ -3,12 +3,17 @@ import type { Provider } from '../types';
 
 interface UseUnConnectedTranferModal {
     open: boolean;
+    setOpen: (open: boolean) => void;
     unConnectedProviderId: Provider | undefined;
 }
 
-const useUnConnectedTranferModal = create<UseUnConnectedTranferModal>(() => ({
+const useUnConnectedTranferModal = create<UseUnConnectedTranferModal>((set) => ({
 
     open: false,
+
+    setOpen(open) {
+        set({ open });
+    },
 
     unConnectedProviderId: undefined,
 
